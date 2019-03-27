a3aa1dfb16d99b0327bf948a98cb97fd5632199f
vagrant <vagrant@vagrant.vm>
2019-03-27 15:54:44 +0000

    added serializer for jsonifying in database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -14,6 +14,14 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+    
+        }
+
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
